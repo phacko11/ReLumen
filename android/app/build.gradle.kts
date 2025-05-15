ed382af24c07e666f7047af4a05132ad3ee3f129
Phan Nguyễn Hữu Phước <138502336+phacko11@users.noreply.github.com>
2025-05-15 13:57:03 +0700

    connect firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,4 +43,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+  implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+  implementation("com.google.firebase:firebase-analytics")
+
 }
